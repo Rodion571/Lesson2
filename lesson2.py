f45592f2b8e356ce42e8bace5615a5aaaa12c9d3
Rodion <rodionbulgakov20@gmail.com>
2024-07-03 14:43:10 +0300

    add lesson2

---
+++ lesson2.py
@@ -0,0 +1,24 @@
+# Task 1
+x = int(input("Number"))
+y = 0
+print(x < y)
+# Tasr 2
+x = int(input("Number"))
+y = 20
+print(x<y)
+# Task 3
+x = int(input("Number"))
+y = 0
+print(x==y)
+# Task 4
+number = int(input("Введіть ціле число: "))
+if number % 2 == 0:
+    print("Парне")
+else:
+    print("Непарне")
+
+# Task 5
+x1 = int(input("Number 1"))
+x2 = int(input("Number 2"))
+x3 = int(input("Number 3"))
+print(max(x1, x2, x3))
